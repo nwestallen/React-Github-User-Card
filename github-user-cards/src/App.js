a93e44f9ee5b6a103d6c30f1f0568ebf93b16279
Nick Allen <nwestallen@gmail.com>
2021-02-09 16:40:54 -0500

    initial app setup

---
+++ github-user-cards/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.setState = {};
+  };
+
+  render() {
+    return(
+      <div>
+        <h1>GitHub People</h1>
+      </div>
+    )
+  }
+};
+
+
+export default App;
