b6d0dc1e53f8f0d98584d1dd1826aa93a29e5b14
Nick Allen <nwestallen@gmail.com>
2021-02-09 21:00:52 -0500

    rendered a UserCard for each github follower

--- github-user-cards/src/App.js
+++ github-user-cards/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import { render } from 'react-dom';
 import axios from 'axios';
 
 import UserCard from './components/UserCard';
@@ -8,26 +7,44 @@ import UserCard from './components/UserCard';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      users: [],
+    };
   };
   
   componentDidMount() {
     axios.get('https://api.github.com/users/nwestallen')
     .then(res => {
-      this.setState(res.data);
+      this.setState({users:[res.data]});
       console.log(this.state);
     })
     .catch(err => console.log(err));
+
+    axios.get('https://api.github.com/users/nwestallen/followers')
+    .then(res => {
+      const followers = res.data;
+      followers.forEach(follower => {
+        axios.get(`https://api.github.com/users/${follower.login}`)
+        .then(response => this.setState({users: [...this.state.users, response.data]}))
+        .catch(error => console.log(error));
+      });
+      console.log(this.state)
+    })
+    .catch(err => console.log(err));
+
     return null;
   }
 
   render() {
+    if(this.state.users.length === 0) {
+      return <h1>Loading</h1>
+    }
     return(
       <div className='container'>
         <div className='header'>
           <h1>GitHub People</h1>
         </div>
-        <UserCard userData={this.state}/>
+        {this.state.users.map(user => <UserCard key={user.id} userData={user}/>)}
       </div>
     )
   }
